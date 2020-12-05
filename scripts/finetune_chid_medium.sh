a6018eba1fca30fe02585a7e6539ece6f7d30291
顾煜贤 <gu-yx17@mails.tsinghua.edu.cn>
2020-12-05 11:28:15 +0800

    meidum small

---
+++ scripts/finetune_chid_medium.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+DATA_DIR="/data/gyx/chid/preprocessed_qa_cands_end"
+CHECKPOINT_PATH="/mnt/nfs/home/zzy/checkpoints/CPM-medium"
+RESULTS_DIR="results"
+MODEL_NAME="finetune-test-medium"
+MPSIZE=1
+NLAYERS=24
+NHIDDEN=1024
+NATT=16
+MAXSEQLEN=1024
+
+CUR_PATH=$(realpath $0)
+CUR_DIR=$(dirname ${CUR_PATH})
+DS_CONFIG="${CUR_DIR}/ds_finetune_medium.json"
+
+python3 -m torch.distributed.launch --master_port ${1-1122} --nproc_per_node 4 finetune_chid.py \
+       --do_train \
+       --do_eval \
+       --data_dir ${DATA_DIR} \
+       --model-parallel-size ${MPSIZE} \
+       --num-layers ${NLAYERS} \
+       --hidden-size ${NHIDDEN} \
+       --load ${CHECKPOINT_PATH} \
+       --num-attention-heads ${NATT} \
+       --seq-length ${MAXSEQLEN} \
+       --max-position-embeddings 1024 \
+       --tokenizer-type GPT2BPETokenizer \
+       --fp16 \
+       --out-seq-length 512 \
+       --tokenizer-path ~/bpe/bpe_3w/ \
+       --vocab-size 30000 \
+       --lr 0.00001 \
+       --warmup 0.1 \
+       --batch-size 2 \
+       --deepspeed \
+       --deepspeed_config ${DS_CONFIG} \
+       --log-interval 1 \
+       --eval-interval 3 \
+       --seed 23333 \
+       --results_dir ${RESULTS_DIR} \
+       --model_name ${MODEL_NAME} \
+       --epoch 1 \
